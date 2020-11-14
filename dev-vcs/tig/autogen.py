f0cbe9cb962c4ced3b0e287f04c361aa26bc2c34
Daniel Robbins <drobbins@funtoo.org>
2020-11-13 19:14:31 -0700

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      faustoo: b947e25f0e440b03f4295cc9df3192d9e3bac300
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: ff3b10d75d63142bc6e69a24f7ac6ec12a4afd56

--- dev-vcs/tig/autogen.py
+++ dev-vcs/tig/autogen.py
@@ -5,27 +5,27 @@ import json
 
 async def generate(hub, **pkginfo):
 
-	github_user = 'jonas'
-	github_repo = pkginfo['name']
+	github_user = "jonas"
+	github_repo = pkginfo["name"]
 	json_list = await hub.pkgtools.fetch.get_page(
-		f'https://api.github.com/repos/{github_user}/{github_repo}/releases', is_json=True
+		f"https://api.github.com/repos/{github_user}/{github_repo}/releases", is_json=True
 	)
 
 	for rel in json_list:
-		name = rel['name']
-		if rel['draft'] == False and rel['prerelease'] == False and name.startswith('tig-'):
+		name = rel["name"]
+		if rel["draft"] == False and rel["prerelease"] == False and name.startswith("tig-"):
 			break
 
-	final_name = f'{name}.tar.gz'
-	version = name.lstrip('tig-')
-	url = f'https://github.com/{github_user}/{github_repo}/releases/download/{name}/{final_name}'
+	final_name = f"{name}.tar.gz"
+	version = name.lstrip("tig-")
+	url = f"https://github.com/{github_user}/{github_repo}/releases/download/{name}/{final_name}"
 
 	ebuild = hub.pkgtools.ebuild.BreezyBuild(
 		**pkginfo,
 		version=version,
 		github_user=github_user,
 		github_repo=github_repo,
-		artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)]
+		artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)],
 	)
 	ebuild.push()
 
--- sys-apps/lsd/autogen.py
+++ sys-apps/lsd/autogen.py
@@ -5,19 +5,19 @@ import toml
 
 
 async def get_crates_artifacts(hub, github_user, github_repo, crate_name, version):
-	crates_raw = await hub.pkgtools.fetch.get_page(f"https://github.com/{github_user}/{github_repo}/raw/{version}/Cargo.lock")
+	crates_raw = await hub.pkgtools.fetch.get_page(
+		f"https://github.com/{github_user}/{github_repo}/raw/{version}/Cargo.lock"
+	)
 	crates_dict = toml.loads(crates_raw)
 	crates = ""
 	crates_artifacts = []
 	for package in crates_dict["package"]:
 		if package["name"] == crate_name:
 			continue
-		crates = crates + package["name"] + "-" + package["version"] + '\n'
+		crates = crates + package["name"] + "-" + package["version"] + "\n"
 		crates_url = "https://crates.io/api/v1/crates/" + package["name"] + "/" + package["version"] + "/download"
 		crates_file = package["name"] + "-" + package["version"] + ".crate"
-		crates_artifacts.append(
-			hub.pkgtools.ebuild.Artifact(url=crates_url, final_name=crates_file)
-		)
+		crates_artifacts.append(hub.pkgtools.ebuild.Artifact(url=crates_url, final_name=crates_file))
 	return dict(crates=crates, crates_artifacts=crates_artifacts)
 
 
@@ -42,10 +42,7 @@ async def generate(hub, **pkginfo):
 		crates=artifacts["crates"],
 		github_user=github_user,
 		github_repo=github_repo,
-		artifacts=[
-			hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name),
-			*artifacts["crates_artifacts"],
-		],
+		artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name), *artifacts["crates_artifacts"],],
 	)
 	ebuild.push()
 
