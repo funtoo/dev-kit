38aa8607c947980b525a3fe39325264896b3d336
Daniel Robbins <drobbins@funtoo.org>
2021-09-19 11:49:52 -0600

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      faustoo: 2e6c2fd9b73c308bedaa32beb93bf80b91818170
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: df7a026691238ca6e91da514d150c4f1e2dff727

--- dev-libs/libvterm/autogen.py
+++ dev-libs/libvterm/autogen.py
@@ -1,26 +1,39 @@
 #!/usr/bin/env python3
 
 import re
+from packaging import version
 from bs4 import BeautifulSoup
 
 
 async def generate(hub, **pkginfo):
-	name = pkginfo["name"]
-	homepage_data = await hub.pkgtools.fetch.get_page("http://www.leonerd.org.uk/code/libvterm/")
-	homepage_soup = BeautifulSoup(homepage_data, "html.parser")
-	name_pattern = re.compile(f"({name}-(.*)\\.tar\\.gz)")
-	for link in homepage_soup.find_all("a", href=True):
-		curr_href = link["href"]
-		curr_match = name_pattern.match(curr_href)
-		if curr_match:
-			latest_version = curr_match
-			break
-	if not latest_version:
-		raise hub.pkgtools.ebuild.BreezyError(f"Can't find a suitable release for {name}")
-	filename, version = latest_version.groups()
-	ebuild = hub.pkgtools.ebuild.BreezyBuild(
-		**pkginfo,
-		version=version,
-		artifacts=[hub.pkgtools.ebuild.Artifact(url=f"http://www.leonerd.org.uk/code/libvterm/{filename}")],
-	)
-	ebuild.push()
+    name = pkginfo["name"]
+    homepage_data = await hub.pkgtools.fetch.get_page(
+        "http://www.leonerd.org.uk/code/libvterm/"
+    )
+    homepage_soup = BeautifulSoup(homepage_data, "html.parser")
+
+    name_pattern = re.compile(f"({name}-(.*)\\.tar\\.gz)")
+
+    link_matches = (
+        name_pattern.match(link.get("href"))
+        for link in homepage_soup.find_all("a", href=True)
+    )
+
+    valid_matches = (match.groups() for match in link_matches if match)
+    release_matches = (match for match in valid_matches if "rc" not in match[1].lower())
+
+    target_filename, target_version = max(
+        release_matches, key=lambda match: version.parse(match[1])
+    )
+
+    ebuild = hub.pkgtools.ebuild.BreezyBuild(
+        **pkginfo,
+        version=target_version,
+        artifacts=[
+            hub.pkgtools.ebuild.Artifact(
+                url=f"http://www.leonerd.org.uk/code/libvterm/{target_filename}"
+            )
+        ],
+    )
+    ebuild.push()
+
