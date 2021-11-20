d31488afb0ddca1ecd8080a884f1aa403be58b44
Daniel Robbins <drobbins@funtoo.org>
2021-11-19 17:41:27 -0700

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      faustoo: a34a4d29dc42f08e738b4d257c56f82b600d6d48
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: 3ebd86a22fb0c0e8927bbf68c1ae7113868a764a

---
+++ dev-libs/protobuf/generators/protobuf-c.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from packaging import version
+
+
+def get_release(releases_data):
+	releases = list(filter(lambda x: x["prerelease"] is False and x['draft'] is False, releases_data))
+	return None if not releases else sorted(releases, key=lambda x: version.parse(x['tag_name'])).pop()
+
+
+async def generate(hub, **pkginfo):
+	github_user = "protobuf-c"
+	github_repo = "protobuf-c"
+	json_list = await hub.pkgtools.fetch.get_page(
+		f"https://api.github.com/repos/{github_user}/{github_repo}/releases", is_json=True
+	)
+	latest_release = get_release(json_list)
+	if latest_release is None:
+		raise hub.pkgtools.ebuild.BreezyError(f"Can't find a suitable release of {github_repo}")
+	version = latest_release['tag_name'].lstrip("v")
+	url = f"https://github.com/{github_user}/{github_repo}/archive/{latest_release['tag_name']}.tar.gz"
+	final_name = f"{pkginfo['name']}-{version}.tar.gz"
+	src_artifact = hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=version,
+		github_user=github_user,
+		github_repo=github_repo,
+		artifacts=[src_artifact]
+	)
+	ebuild.push()
