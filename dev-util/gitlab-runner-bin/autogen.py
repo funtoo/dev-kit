8beca5b71bf10c2491d39ecebb39ea997fc8f026
Daniel Robbins <drobbins@funtoo.org>
2022-02-22 09:12:42 -0700

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      faustoo: 0977301b537a77c81e5e8d2dd326361bc6f12523
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: 28043f682ca5231e20d3478cb5f45338887e3b0e

--- dev-util/gitlab-runner-bin/autogen.py
+++ dev-util/gitlab-runner-bin/autogen.py
@@ -1,21 +1,23 @@
-#!/usr/bin/env python3
-
 async def generate(hub, **pkginfo):
-  github_user = "gitlabhq"
-  github_repo = "gitlab-runner"
-  json_list = await hub.pkgtools.fetch.get_page(
-      f"https://api.github.com/repos/{github_user}/{github_repo}/tags", is_json=True
-  )
-  latest = json_list[0]
-  version = latest["name"][1:]
+    gitlab_user = "gitlab-org"
+    gitlab_repo = "gitlab-runner"
 
-  url = f"https://gitlab-runner-downloads.s3.amazonaws.com/v{version}/binaries/gitlab-runner-linux-amd64"
-  final_name = f'{pkginfo["name"]}-{version}.tar.gz'
-  ebuild = hub.pkgtools.ebuild.BreezyBuild(
-      **pkginfo,
-      github_user=github_user,
-      github_repo=github_repo,
-      version=version,
-      artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)],
-  )
-  ebuild.push()
+    json_list = await hub.pkgtools.fetch.get_page(
+        f"https://gitlab.com/api/v4/projects/{gitlab_user}%2F{gitlab_repo}/releases", is_json=True
+    )
+    
+    latest = json_list[0]
+    version = latest["name"][1:]
+    
+    url = f"https://gitlab.com/gitlab-org/gitlab-runner/-/releases/v{version}/downloads/binaries/gitlab-runner-linux-amd64"
+    
+    final_name = f'{pkginfo["name"]}-{version}.tar.gz'
+    
+    ebuild = hub.pkgtools.ebuild.BreezyBuild(
+        **pkginfo,
+        version=version,
+        artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)],
+    )
+    
+    ebuild.push()
+
