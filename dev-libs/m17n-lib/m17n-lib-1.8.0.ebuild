56ef9a63ce3f37415f493410818a94ffad5c0632
Daniel Robbins <drobbins@funtoo.org>
2022-08-21 14:27:16 -0600

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      faustoo: 808a41a94efd78d19605d8e326b177257bbda4ce
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: be794cfb37648127d262d561a288dd638af49b93

--- dev-libs/m17n-lib/m17n-lib-1.8.0.ebuild
+++ dev-libs/m17n-lib/m17n-lib-1.8.0.ebuild
@@ -1,4 +1,3 @@
-# Copyright 1999-2021 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI="7"
@@ -84,3 +83,4 @@ src_configure() {
 
     econf "${myconf[@]}"
 }
+
--- dev-libs/m17n/autogen.py
+++ dev-libs/m17n/autogen.py
@@ -4,29 +4,40 @@ from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 
 archive = ".tar.gz"
-pkg_name = "m17n-contrib"
+pkg_category = 0
+pkg_name = 1
 url_g = "https://download.savannah.nongnu.org/releases/m17n/"
 
-async def get_latest_release(hub, **pkginfo):
+async def get_latest_release(package, hub, **pkginfo):
     # Here we get the site, sorted by latest date on the top
     html = await hub.pkgtools.fetch.get_page(f"{url_g}?C=M&O=D")
     # Iterate all links in the page
     for a in BeautifulSoup(html, features="html.parser").find_all("a", href=True):
         href = a['href']
         # Check if the link begins with m17n-contrib, ends with .tar.gz and doesn't contain RC(Release Candidate) in the name
-        if href.endswith(archive) and href.startswith(pkg_name) and href.find("RC") == -1:
+        if href.endswith(archive) and href.startswith(package[pkg_name]) and href.find("RC") == -1:
             return href
 
-async def generate(hub, **pkginfo):
-    result = await get_latest_release(hub, **pkginfo)
+async def submit(package, hub, **pkginfo):
+    result = await get_latest_release(package, hub, **pkginfo)
     
     # At the end there is a +1 because we need to remove the - before the version number
-    version = result[:len(result) - len(archive)][len(pkg_name) + 1:]
+    version = result[:len(result) - len(archive)][len(package[pkg_name]) + 1:]
     url = f"{url_g}{result}"
+
+    pkginfo["cat"] = package[pkg_category]
+    pkginfo["name"] = package[pkg_name]
+    tmpl_name = package[pkg_name] + ".tmpl"
 
     ebuild = hub.pkgtools.ebuild.BreezyBuild(
         **pkginfo,
+        template=tmpl_name,
         version=version,
-        artifacts=[hub.pkgtools.ebuild.Artifact(url=url, finalname=result)],
+        artifacts=[hub.pkgtools.ebuild.Artifact(url=url)],
     )
     ebuild.push()
+
+async def generate(hub, **pkginfo):
+    packages = [[ "dev-db", "m17n-db" ], [ "dev-db", "m17n-contrib"], [ "dev-libs", "m17n-lib" ]]
+    for package in packages:
+        await submit(package, hub, **pkginfo)
