b22f53909fa9c5f367284be2027d82d3ff1d18f8
Daniel Robbins <drobbins@funtoo.org>
2022-08-22 09:15:32 -0600

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      faustoo: 808a41a94efd78d19605d8e326b177257bbda4ce
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: 9484e08a69b649872c792634938918ae7fd1504f

---
+++ dev-libs/oniguruma/autogen.py
@@ -0,0 +1,27 @@
+from packaging.specifiers import SpecifierSet
+from packaging.version import Version
+
+async def generate(hub, **pkginfo):
+	github_user = "kkos"
+	github_repo = "oniguruma"
+	json_list = await hub.pkgtools.fetch.get_page(
+		f"https://api.github.com/repos/{github_user}/{github_repo}/releases", is_json=True
+	)
+
+	for rel in json_list:
+		version = rel["name"].replace("Release ", "")
+		if 'tarball_url' in rel:
+			url = rel['tarball_url']
+
+		if version != "":
+			break
+
+	url=f"https://github.com/{github_user}/{github_repo}/releases/download/v{version}/onig-{version}.tar.gz"
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		version=version,
+		github_user=github_user,
+		github_repo=github_repo,
+		artifacts=[hub.pkgtools.ebuild.Artifact(url=url)],
+	)
+	ebuild.push()
