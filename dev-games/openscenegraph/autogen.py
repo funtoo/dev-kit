1f0f4a953114382a6b556b93883b16e7016696db
Daniel Robbins <drobbins@funtoo.org>
2023-04-18 13:58:04 +0000

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      faustoo: e5d43ed5508e6b52e50e663f8e884be9e39481fc
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: 54c5aa8049f0954a81aaf85223cf0185fec10574

---
+++ dev-games/openscenegraph/autogen.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from packaging.version import Version
+from os import path
+
+async def generate(hub, **pkginfo):
+	github_user = pkginfo["name"]
+	github_repo = "OpenSceneGraph"
+	select = f"{github_repo}-\d+[\.\d]+"
+
+	newpkginfo = await hub.pkgtools.github.tag_gen(hub, github_user, github_repo, select=select)
+	pkginfo.update(newpkginfo)
+
+	artifact = pkginfo['artifacts'][0]
+	sha = pkginfo['sha'][:7]
+	await artifact.fetch()
+	artifact.extract()
+
+	cmfile = path.join(artifact.extract_path, f"{github_user}-{github_repo}-{sha}", "CMakeLists.txt")
+	with open(cmfile, "r") as cmf:
+		for line in cmf.readlines():
+			if 'SOVERSION' in line:
+				soversion = line.split()[1].split(')')[0] # remove trailing closing paren
+				break
+
+	ebuild = hub.pkgtools.ebuild.BreezyBuild(
+		**pkginfo,
+		github_user=github_user,
+		github_repo=github_repo,
+		slot=soversion
+	)
+	ebuild.push()
+
+
+# vim: ts=4 sw=4 noet
